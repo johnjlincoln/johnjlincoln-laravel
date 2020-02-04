ba2825c7c9538ff22716163a265649647d41a70b
John Lincoln <jlincoln88@gmail.com>
2020-02-04 00:01:57 -0500

    productionize app to run on heroku (#3)

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -10,14 +10,25 @@ class TrustProxies extends Middleware
     /**
      * The trusted proxies for this application.
      *
+     * Since Heroku uses various internal load balancers for routing requests,
+     * and since those load balancers always set X-Forwarded headers, we can
+     * therefor trust all proxies.
+     *
+     * TODO: conditionally set trusted proxies based on Heroku environment detection.
+     *
      * @var array|string
      */
-    protected $proxies;
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
      *
+     * Since we're deploying on Heroku, and since Heroku load balancers are
+     * AWS Elastic Load Balancers, we will configure Laravel to use AWS header.
+     *
+     * TODO: conditionally set protected headers based on Heroku environment detection.
+     *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers = Request::HEADER_X_FORWARDED_AWS_ELB;
 }
